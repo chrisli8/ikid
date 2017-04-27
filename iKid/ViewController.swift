38f125f09d6558192ad0e259e65f7e72b6f344c6
chrisli8 <8chrisgli@gmail.com>
2017-04-26 21:28:01 -0700

    tab bar working to go to different views

---
+++ iKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Chris Li on 4/26/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
